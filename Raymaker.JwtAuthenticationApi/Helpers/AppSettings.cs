0ecc631e2ededd7a2d2c04b7f8442a5f55b182f3
Jens Rud Nielsen <jenrud@gmail.com>
2021-09-12 17:40:05 +0200

    Adds another project with JWT middleware example

---
+++ Raymaker.JwtAuthenticationApi/Helpers/AppSettings.cs
@@ -0,0 +1,7 @@
+﻿namespace Raymaker.JwtAuthenticationApi.Helpers
+{
+    public class AppSettings
+    {
+        public string Secret { get; set; }
+    }
+}
---
+++ Raymaker.JwtAuthenticationApi/Startup.cs
@@ -0,0 +1,60 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
+using Raymaker.JwtAuthenticationApi.Helpers;
+using Raymaker.JwtAuthenticationApi.Services;
+
+namespace Raymaker.JwtAuthenticationApi
+{
+    public class Startup
+    {
+        public IConfiguration Configuration { get; }
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Raymaker.JwtAuthenticationApi", Version = "v1" });
+            });
+
+            // configure strongly typed settings object
+            services.Configure<AppSettings>(Configuration.GetSection("AppSettings"));
+
+            // configure DI for application services
+            services.AddScoped<IUserService, UserService>();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Raymaker.JwtAuthenticationApi v1"));
+            }
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            // custom jwt auth middleware
+            app.UseMiddleware<JwtMiddleware>();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
